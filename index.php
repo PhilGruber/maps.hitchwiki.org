5d738b4502f97e5948bdb3317e7f8b89b75bf8c6
MrTweek <mrtweek@flupps.net>
2010-05-19 16:10:30 +0200

    got json-rpc running

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<h1>New super cool and fast hitchwiki maps!!!!!!1111</h1>
-<br /><br /><br />
-<small>...coming soon</small>
+<?php 
+
+require_once "rpc.php";
+
+?><html>
+    <head>
+        <?php $server->javascript("rpc"); ?>
+    </head>
+    <body onload='alert(rpc.say("Test"));'>
+        <h1>New super cool and fast hitchwiki maps!!!!!!1111</h1>
+        <br /><br /><br />
+        <small>...coming soon</small>
+    </body>
+</html>
