c8a6c6f8bc8a0ff599e14dcc6f036d72757827e2
MrTweek <mrtweek@flupps.net>
2010-05-19 19:44:29 +0200

    first steps

--- main.js
+++ main.js
@@ -2,11 +2,29 @@
 
 function init() {
     var map = new OpenLayers.Map('map');
-    var wms = new OpenLayers.Layer.WMS(
-        "OpenLayers WMS",
-        "http://labs.metacarta.com/wms/vmap0",
-        {'layers':'basic'});
-    map.addLayer(wms);
-    map.zoomToMaxExtent();
+    
+    map.addLayer(new OpenLayers.Layer.OSM());
+    map.addLayer(new OpenLayers.Layer.VirtualEarth());
+    map.addLayer(new OpenLayers.Layer.Yahoo());
+    // map.addLayer(new OpenLayers.Layer.Google("Google"));
+
+    var panelControls = [
+        new OpenLayers.Control.Navigation(),
+        new OpenLayers.Control.LayerSwitcher()
+    ];
+
+    var toolbar = new OpenLayers.Control.Panel({
+        displayClass: 'olControlEditingToolbar',
+        defaultControl: panelControls[0]
+    });
+
+    toolbar.addControls(panelControls);
+    map.addControl(toolbar);
+    
+    map.setCenter(new OpenLayers.LonLat(49, 8.3));
+    map.zoomTo(3);
+
+    var test = rpc.getMarkers(49, 8.3, 3);
+    alert(test);
 }
 
--- rpc.php
+++ rpc.php
@@ -6,6 +6,15 @@ class rpc {
     function say($str) {
         return "You said $str!";
     }
+
+    function getMarkers($lat, $lng, $count) {
+        $res = array();
+        while ($count--) {
+            $res[$count] = array(rand(12000)/1000-60, rand(36000)/1000-180);
+        }
+        return "lululul";
+        return $res;
+    }
 }
 
 $server = new JSONRpcServer(new rpc());
